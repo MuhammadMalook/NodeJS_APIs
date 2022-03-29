90823b1809379e8ce26c19ef93c8439067f14402
Muhammad Malook <maharmohammadmalook@gmail.com>
2022-03-29 08:58:28 +0500

    post api added

--- app.js
+++ app.js
@@ -4,10 +4,28 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
-app.post('/', (req,res)=>{
+app.get('/', (req,res)=>{
     // console.log('Got body:', req.body);
     // res.sendStatus(200);
-    res.send("req.body")
+    res.send("hello world");
 });
+
+const courses = [
+    {id:1, name:"Programming Fundamentals"},
+    {id:2, name:"Object Oriented Programming"}
+]
+
+app.get('/api/courses' , (req, res)=>{
+        res.send(courses)
+});
+
+app.post('/api/courses', (req, res)=>{
+    const course = {
+        id: courses.length+1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(courses);
+})
 
 app.listen(3000, ()=> console.log("Server is listening to port 3000"))
